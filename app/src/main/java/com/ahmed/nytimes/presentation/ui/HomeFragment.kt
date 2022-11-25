aebddd21f0788e8c94b8549fc9b6d488eca99d78
ahmedmohamed299 <ahmed_mohamed299@hotmail.com>
2022-11-25 02:17:29 +0200

    Update HomeFragment.kt

--- app/src/main/java/com/ahmed/nytimes/presentation/ui/HomeFragment.kt
+++ app/src/main/java/com/ahmed/nytimes/presentation/ui/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.ahmed.nytimes.presentation.ui
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
